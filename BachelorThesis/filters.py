2542d721dee6defdeacf0a070c90f642ebca7cd6
Nicklas Hansen <nicklash@hotmail.dk>
2018-04-19 20:28:57 +0200

    random stuff

--- BachelorThesis/filters.py
+++ BachelorThesis/filters.py
@@ -1,6 +1,7 @@
 from numpy import *
 from sklearn.preprocessing import quantile_transform
 from scipy.signal import medfilt
+from sklearn.preprocessing import MinMaxScaler
 
 """
 WRITTEN BY
@@ -8,15 +9,26 @@ Nicklas Hansen
 """
 
 def median_filt(X, kernel = 3):
-	return operation(X, medfilt, (3))
+	return operation_1D(X, medfilt, (kernel,0))
 
 def quantile_norm(X, quantiles = 10):
-	return operation(X, quantile_transform, (0,10))
+	return operation_2D(X, quantile_transform, (0,quantiles))
 
-def operation(X, op=quantile_transform, args=None):
+def operation_1D(X, op=median_filt, args=None):
 	_X = transpose(X)
 	for i in range(1,len(_X)-1):
-		if (len(args) > 1):
+		if (args == None):
+			_X[i] = op(_X[i])
+		else:
+			_X[i] = op(_X[i], args[0])
+	return transpose(_X)
+
+def operation_2D(X, op=quantile_transform, args=None):
+	_X = transpose(X)
+	for i in range(1,len(_X)-1):
+		if (args == None):
+			_X[i] = squeeze(op(reshape(_X[i], (-1,1))))
+		elif (args[0] == 0):
 			_X[i] = squeeze(op(reshape(_X[i], (-1,1)), args[0], args[1]))
 		else:
 			_X[i] = squeeze(op(reshape(_X[i], (-1,1)), args[0]))
