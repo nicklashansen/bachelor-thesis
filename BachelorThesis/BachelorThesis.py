c7369ee308d2b4181b4fceaa552b0070b96e38f4
Nicklas Hansen <nicklash@hotmail.dk>
2018-05-08 10:48:06 +0200

    initial test

--- BachelorThesis/BachelorThesis.py
+++ BachelorThesis/BachelorThesis.py
@@ -13,8 +13,8 @@ Michael Kirkegaard
 """
 
 if __name__ == '__main__':
-	#dataflow()
+	dataflow()
 	#BachelorGUI()
-	fit_eval(gpu=False)
+	#fit_eval(gpu=False)
 	#process_epochs()
 	#prepAll(force=False)
--- BachelorThesis/dataflow.py
+++ BachelorThesis/dataflow.py
@@ -16,18 +16,28 @@ Nicklas Hansen
 Michael Kirkegaard
 """
 
-def dataflow(filename = 'mesa-sleep-0052'):
+def dataflow(filename = 'mesa-sleep-2084'):
 	epoch_length, overlap_factor, sample_rate = 120, 2, 256
 	#X,y = prepSingle(filename, save=False)
 	X,y = fs.load_csv(filename)
-	epochs = epochs_from_prep(X, y, epoch_length, overlap_factor, filter = True, removal=True)
+	epochs = epochs_from_prep(X, y, epoch_length, overlap_factor, filter = False, removal=True)
 	full = epochs_from_prep(X, y, epoch_length, overlap_factor, filter = False, removal=False)
 	model = gru(dataset(epochs))
 	model.graph = load_model('gru.h5')
 	epochs = model.predict(epochs)
 	epochs.sort(key=lambda x: x.index_start, reverse=False)
 	full.sort(key=lambda x: x.index_start, reverse=False)
-	yhat, wake, rem, illegal = timeseries(epochs, full, epoch_length, overlap_factor, sample_rate)
+	results = model.evaluate(epochs)
+	ya, yhat, wake, rem, illegal = timeseries(epochs, full, epoch_length, overlap_factor, sample_rate)
+
+	results = metrics.compute_scores(ya, yhat)['cm_overlap']
+	del results['mcc']
+	del results['precision']
+	del results['specificity']
+	del results['TP_FP_TN_FN']
+	del results['accuracy']
+	print(results)
+
 	print('Evaluated from', int(epochs[0].index_start/sample_rate), ' s to', int(epochs[-1].index_stop/sample_rate), 's')
 	ill = region(illegal)
 	ill.append([0, int(full[0].index_start/sample_rate)])
@@ -41,21 +51,21 @@ def epochs_from_prep(X, y, epoch_length=epoch.EPOCH_LENGTH, overlap_factor=epoch
 def timeseries(epochs, full, epoch_length, overlap_factor, sample_rate):
 	window = int(epoch_length - ( epoch_length / overlap_factor))
 	length = int(full[-1].index_stop/sample_rate)
-	yhat, wake, rem, illegal = zeros(length), zeros(length), zeros(length), zeros(length)
+	y, yhat, wake, rem, illegal = zeros(length), zeros(length), zeros(length), zeros(length), zeros(length)
 	for i,obj in enumerate(epochs):
+		y = modify_timeseries(y, obj.y, 1, obj.timecol, window, sample_rate)
 		yhat = modify_timeseries(yhat, obj.yhat, 1, obj.timecol, window, sample_rate)
 	for i,obj in enumerate(full):
 		sleep = transpose(obj.X)[-1]
 		wake = modify_timeseries(wake, sleep, -1, obj.timecol, window, sample_rate)
 		rem = modify_timeseries(rem, sleep, 1, obj.timecol, window, sample_rate)
-	for i,obj in enumerate(full):
 		illegal = modify_timeseries(illegal, obj.mask, 1, obj.timecol, window, sample_rate)
 	for i in range(len(wake)):
-		if yhat[i] == 1 and wake[i] == 1:
-			yhat[i] = 0
+		#if yhat[i] == 1 and wake[i] == 1:
+		#	yhat[i] = 0
 		if illegal[i] == 1 and wake[i] == 1:
 			illegal[i] = 0
-	return yhat, wake, rem, illegal
+	return y, yhat, wake, rem, illegal
 
 def modify_timeseries(ts, values, criteria, timecol, window, sample_rate):
 	for i,y in enumerate(values[window:]):
@@ -164,8 +174,7 @@ def fit_eval(gpu = True):
 	batch_size = 2 ** 10 if gpu else 2 ** 7
 	model, test = fit(batch_size)
 	model.save()
-	#score = model.evaluate(test)
-	#print(score)
+	#results = eval(model, test)
 
 def fit(batch_size = 100):
 	data = dataset(fs.load_epochs(), balance = True)
@@ -175,6 +184,13 @@ def fit(batch_size = 100):
 	model = gru(data, batch_size)
 	model.fit(train)
 	return model, test
+
+def eval():
+	model = gru(dataset(epochs))
+	model.graph = load_model('gru.h5')
+	results = model.evaluate(epochs)
+	print(results)
+	return results
 
 class dataset:
 	def __init__(self, epochs, shuffle=True, balance=True):
