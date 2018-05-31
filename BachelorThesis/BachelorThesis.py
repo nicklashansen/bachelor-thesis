780aea9cae8b3d14bb489d431ae5a33dd6087046
Nicklas Hansen <nicklash@hotmail.dk>
2018-05-31 14:04:58 +0200

    forced commit

--- BachelorThesis/BachelorThesis.py
+++ BachelorThesis/BachelorThesis.py
@@ -14,9 +14,14 @@ Michael Kirkegaard
 
 if __name__ == '__main__':
 	#parameter_tuning(evaluate_model=False)
-	#test_bidirectional()
+	#train_validate_test()
 	#fit_validate_test(only_arousal = True)
-	evaluate(validation=False, path='gru.h5')
+	#evaluate(validation=False, path='FEATURE_SELECTION\\best_rr.h5')
+	#evaluate(validation=False, path='FEATURE_SELECTION\\best_rwa.h5')
+	evaluate(validation=False, path='FEATURE_SELECTION\\best_ecg.h5')
+	#evaluate(validation=False, path='FEATURE_SELECTION\\best_ppg.h5')
+	#evaluate(validation=False, path='FEATURE_SELECTION\\best_rr_ppg.h5')
+	#evaluate(validation=False, path='FEATURE_SELECTION\\best_rwa_ppg.h5')
 	#test_dataflow(file='mesa-sleep-1541')
 	#count_epochs_removed()
 	#test_dataflow_LR()
